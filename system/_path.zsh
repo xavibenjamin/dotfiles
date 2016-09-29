2e8b4258745cca0eebde0f74226946a6dc64cffd
Tim Smith <smith@ttimsmith.com>
2016-09-28 23:18:24 -0500

    Fix path so that my executables work

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,6 +1,8 @@
-export PATH="./bin:~/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
-
+# Boxen for ACL apps
 export BOXEN_SOCKET_DIR=/usr/local/var/project-sockets
 
-### Added by the Heroku Toolbelt
+# Heroku Toolbelt Path
 export PATH="/usr/local/heroku/bin:$PATH"
+
+# Main Path
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
