467a39eb9fec0f2ea49334e7d290e3412f2523b7
Tim Smith <smith@ttimsmith.com>
2016-09-28 21:44:32 -0500

    add /bin to path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,4 @@
-export PATH="~/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+export PATH="./bin:~/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 
 export BOXEN_SOCKET_DIR=/usr/local/var/project-sockets
 
