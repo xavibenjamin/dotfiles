bbbfae0ae98ad698070c3a82275ac36615ce6380
Tim Smith <smith@ttimsmith.com>
2016-10-03 14:16:05 -0500

    Add more aliases for git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,7 +1,13 @@
 # Git Related Aliases
 
+alias gb='git branch'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias gs='git status -sb'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
