74eb564ee6bb4fbdebdd6e058cb9e69198225504
Timothy B. Smith <smith@ttimsmith.com>
2017-10-05 14:13:54 -0500

    Add new jekyll alias

--- jekyll/aliases.zsh
+++ jekyll/aliases.zsh
@@ -1,4 +1,6 @@
 alias jwd="bundle exec jekyll serve --drafts"
-alias jw="bundle exec jekyll serve -w"
+alias jw="bundle exec jekyll serve"
+alias jw20="bundle exec jekyll serve --limit_posts 20"
+
 
 alias gpmp="git add . && git commit -m 'new microblog post' && gp"
