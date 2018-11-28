da6038eb3bc46a7df2efee05f756446c3b7e6f40
Timothy B. Smith <smith@ttimsmith.com>
2018-11-28 08:54:23 -0600

    Update Jekyll serve aliases to open the browser

--- jekyll/aliases.zsh
+++ jekyll/aliases.zsh
@@ -1,9 +1,7 @@
-alias jwd="bundle exec jekyll serve --limit_posts 10 --future --drafts"
-alias jw="bundle exec jekyll serve"
-alias jw20="bundle exec jekyll serve --limit_posts 20"
+alias jwd="bundle exec jekyll serve --limit_posts 10 --future --drafts -o"
+alias jw="bundle exec jekyll serve -o"
+alias jw20="bundle exec jekyll serve --limit_posts 20 -o"
 
 alias gpmp="git add . && git commit -m 'new microblog post' && gp"
 
-alias jnp="bundle exec jekyll post"
-
-alias jprod="JEKYLL_ENV=production bundle exec jekyll serve --limit_posts 20 --config _config_production.yml"
+alias jprod="JEKYLL_ENV=production bundle exec jekyll serve --limit_posts 20 --config _config_production.yml -o"
