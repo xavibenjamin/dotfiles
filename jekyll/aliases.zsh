334dc9ed05ffc84737914ed01a9835808e77a5e6
Timothy B. Smith <smith@ttimsmith.com>
2018-11-29 23:21:49 -0600

    Add new jekyll-compose aliases

--- jekyll/aliases.zsh
+++ jekyll/aliases.zsh
@@ -1,7 +1,11 @@
+# Building Jekyll Aliases
+
 alias jwd="bundle exec jekyll serve --limit_posts 10 --future --drafts -o"
 alias jw="bundle exec jekyll serve -o"
 alias jw20="bundle exec jekyll serve --limit_posts 20 -o"
+alias jprod="JEKYLL_ENV=production bundle exec jekyll serve --config _config_production.yml -o"
 
-alias gpmp="git add . && git commit -m 'new microblog post' && gp"
+# Jekyll Compose Aliases
 
-alias jprod="JEKYLL_ENV=production bundle exec jekyll serve --limit_posts 20 --config _config_production.yml -o"
+alias jcd="bundle exec jekyll draft"
+alias jcp="bundle exec jekyll publish"
