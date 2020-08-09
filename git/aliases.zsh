5c808c4140e36d4732b90ef48d9a744522b262e5
Timothy B. Smith <tim@smithtimmytim.com>
2020-08-09 15:14:34 -0700

    update git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,14 +1,15 @@
 # Git Related Aliases
 
-alias gb='git branch'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
+alias g='git'
+alias gb='g branch'
+alias gc='g commit'
+alias gca='g commit -a'
+alias gco='g checkout'
 
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an %Cblue<%ae>%Creset: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gp1='git push -u origin HEAD'
-alias gs='git status -sb'
+alias glog="g log --graph --pretty=format:'%Cred%h%Creset %an %Cblue<%ae>%Creset: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='g push origin HEAD'
+alias gp1='g push -u origin HEAD'
+alias gs='g status -sb'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
+alias gd='g diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
