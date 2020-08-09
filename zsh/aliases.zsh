8a433b60b7f35fb84152609c140577f0e90dd07c
Timothy B. Smith <tim@smithtimmytim.com>
2020-08-09 15:14:58 -0700

    Add some navigation aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,28 @@
 alias reload!='. ~/.zshrc'
 alias dotconfig="e ~/.dotfiles"
 alias ps="python3 -m http.server 8000"
+
+
+# Navigation
+alias ..="cd .."
+alias cd..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+
+###
+# time to upgrade `ls`
+
+# use coreutils `ls` if possible…
+hash gls >/dev/null 2>&1 || alias gls="ls"
+
+# always use color, even when piping (to awk,grep,etc)
+if gls --color > /dev/null 2>&1; then colorflag="--color"; else colorflag="-G"; fi;
+export CLICOLOR_FORCE=1
+
+# ls options: A = include hidden (but not . or ..), F = put `/` after folders, h = byte unit suffixes
+alias ls='gls -AFh ${colorflag} --group-directories-first'
+alias lsd='ls -l | grep "^d"' # only directories
+#    `la` defined in .functions
+###
