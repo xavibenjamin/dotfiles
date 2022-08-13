2d3eb0f70b92bfaed18b13338c68eb9e2b70f9ac
Tim Smith <tim@smithtimmytim.com>
2022-08-12 17:25:33 -0700

    update homebrew script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -11,13 +11,7 @@ then
   echo "  Installing Homebrew for you."
 
   # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
-  fi
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 fi
 
