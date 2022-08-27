7369a4bcb63dcedc1795cd763d8b4da35ec2adb5
smithtimmytim <tim@smithtimmytim.com>
2022-08-26 20:00:14 -0700

    update zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,18 +11,16 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
 
-###
-# time to upgrade `ls`
+# grc overides for ls
+#   Made possible through contributions from generous benefactors like
+#   `brew install coreutils`
+if $(gls &>/dev/null)
+then
+  alias ls="gls -AFh --color --group-directories-first"
+  alias l="gls -lAh --color"
+  alias ll="gls -l --color"
+  alias la='gls -A --color'
+fi
 
-# use coreutils `ls` if possible…
-hash gls >/dev/null 2>&1 || alias gls="ls"
-
-# always use color, even when piping (to awk,grep,etc)
-if gls --color > /dev/null 2>&1; then colorflag="--color"; else colorflag="-G"; fi;
-export CLICOLOR_FORCE=1
-
-# ls options: A = include hidden (but not . or ..), F = put `/` after folders, h = byte unit suffixes
-alias ls='gls -AFh ${colorflag} --group-directories-first'
 alias lsd='ls -l | grep "^d"' # only directories
-#    `la` defined in .functions
-###
+
